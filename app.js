9b7bd1aeecd3ceeee1789096e8dc84fdb8f597de
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-09 14:46:47 -0700

    image filters and basic animations

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
+$(document).ready(function() {
+
+
+
+
+
 //this is the random number generator ...
-
-
-
-
 function numberGenerated() {
    return Math.floor(Math.random() * 10);
 }
 
+
+//This places the debris on the right side of the screen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
