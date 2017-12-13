470c2f2aaa1f37c77db4b2344a447bdd42c9d5c1
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-13 12:38:39 -0700

    added dope ass meteor click animations

--- js/app.js
+++ js/app.js
@@ -2,34 +2,36 @@
 
 //Meteor Generator
 
-$('html, body').css({
-   overflow: 'hidden',       //disables scrolling
-   height: '100%',
-   width: '100%',
+//disables scrolling
+$('html, body').css({  
+   overflow: 'hidden', 
+   height: '100%',     
+   width: '100%',     
 });
 
 
-var b = setInterval(generator, 700);
-
-
-
+var a = setInterval(generator, 800);
 
 
 
 // Makes a meteor and drops it into the game !!ONCE PER SECOND!!
 // image class = meteor
 function generator() {
-	var meteorCoordinate = Math.floor(Math.random() * 100) + 30 ; // number from 30-100
-    var meteor = document.createElement("IMG");					// creates image
-    meteor.setAttribute("src", "./gameimages/meteor.png");		// makes image a meteor
-    meteor.setAttribute("class", "meteor");						// sets class
-    meteor.style.paddingLeft = meteorCoordinate +"%";     		// padding left is equal to 30-100
-    document.body.appendChild(meteor);							// puts meteor into the game board
+	var meteorCoordinate = Math.floor(Math.random() * 100) + 5 ; // number from 1-100
+    var meteor = document.createElement("IMG");					        // creates image
+    meteor.setAttribute("src", "./gameimages/meteor.gif");		  // makes image a meteor
+    meteor.setAttribute("class", "meteor");
+    meteor.setAttribute("id", Math.random()); 
+    meteor.setAttribute("onclick", "this.src='./gameimages/sparkle.gif'");
+      				           		// sets class
+    meteor.style.marginLeft = meteorCoordinate +"%";     	   	// padding left is equal to 30-100
+    document.body.appendChild(meteor);							             // puts meteor into the game board
 
 }
 
-//Player Controls
 
+
+//Player Controls
 $(document).keypress(function(a) {
     if(a.keyCode == 97)
 	$(".playerplaceholder").animate({marginTop: "-=3%"}, -200);
@@ -46,16 +48,18 @@ $(document).keyup(function(space) {
 
 
 
-
-function click() {
-   var ammo = document.createElement("IMG");        
-    ammo.setAttribute("src", "./gameimages/ammo.png");    
-    ammo.setAttribute("class", "ammo");
-    ammo.style.paddingLeft = "13%";   
-    document.body.appendChild(ammo);             
-}
+    //If you want to remove all images
+    //$(this).parent().find('img').remove();
 
 
+
+ // function click() {
+//    var ammo = document.createElement("IMG");        
+//     ammo.setAttribute("src", "./gameimages/ammo.png");    
+//     ammo.setAttribute("class", "ammo");
+//     ammo.style.paddingLeft = "13%";   
+//     document.body.appendChild(ammo);             
+// }
 
 
 
