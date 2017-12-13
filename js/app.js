1db43e77f4bb4ce2b576ac4d2b5e4141679fe8e5
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-13 13:14:44 -0700

    changed on click to be a mouseover

--- js/app.js
+++ js/app.js
@@ -10,22 +10,22 @@ $('html, body').css({
 });
 
 
-var a = setInterval(generator, 800);
+var a = setInterval(generator, 600);
+var b = setInterval(generator, 900);
 
 
 
 // Makes a meteor and drops it into the game !!ONCE PER SECOND!!
 // image class = meteor
 function generator() {
-	var meteorCoordinate = Math.floor(Math.random() * 100) + 10 ; // number from 1-100
+	var meteorCoordinate = Math.floor(Math.random() * 80) + 10 ; // number from 1-100
     var meteor = document.createElement("IMG");					        // creates image
     meteor.setAttribute("src", "./gameimages/meteor.gif");		  // makes image a meteor
     meteor.setAttribute("class", "meteor");
     meteor.setAttribute("id", Math.random()); 
-    meteor.setAttribute("onclick", "this.src='./gameimages/sparkle.gif'");
+    meteor.setAttribute("onmouseleave", "this.src='./gameimages/sparkle.gif'");
     meteor.style.marginLeft = meteorCoordinate +"%";
-    meteor.style.padding = 5 +"%";  
-      	   	// padding left is equal to 30-100
+    meteor.style.padding = 3 +"%";  
     document.body.appendChild(meteor);							             // puts meteor into the game board
 
 }
